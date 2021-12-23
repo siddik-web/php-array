c865d3e4fe8c02efd0dd1502992223ecb4c27e96
Md Siddiqur Rahman <siddikcoder@gmail.com>
2021-12-23 18:53:26 +0600

    update application class

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php 
 
+use App\Application;
+
 require_once './vendor/autoload.php';
+
+$app = Application::getApplication();
+
+$app::setName("Md Siddiqur Rahman");
+
+$data = [2,3,4,5];
+$app::addItem($data);
