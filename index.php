b9686dd9a800dcbed23286d6a5733956182caaef
Md Siddiqur Rahman <siddikcoder@gmail.com>
2021-12-31 11:09:06 +0600

    use of array_unique and array_count_values

--- index.php
+++ index.php
@@ -9,7 +9,28 @@ $app = Application::getApplication();
 $app::setName("Md Siddiqur Rahman");
 
 $data = [2,3,4,5, [23,44,55,[44,55,22,[445,622,[555,66],555]]]];
-$app::addItem($data);
+//$app::addItem($data);
 
 // SizeOff
-echo sizeof($data);
+//echo sizeof($data);
+
+
+$num = [1, 2, 3, 4, 5, 6];
+
+// Remove duplicate value;
+$unique = array_unique($num);
+
+echo count($unique) === count($num)? true : false;
+
+// Find Number of times value.
+$unique_value = array_count_values($num);
+
+print_r($unique_value);
+
+
+
+
+
+
+
+
